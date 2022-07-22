7b5a22d02942c8d2a3182cfabd4c85e29e0d2865
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-07-21 23:21:00 -0400

    Time: 126 ms (91.69%), Space: 13.8 MB (82.04%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maximumUnits(self, boxTypes, truckSize):
+        boxTypes.sort(key = lambda x: -x[1])
+        unitCounter = 0
+        for x, y in boxTypes:
+            if truckSize == 0:
+                return unitCounter
+            if truckSize >= x:
+                unitCounter += x * y
+                truckSize -= x
+            elif truckSize < x:
+                unitCounter += (truckSize) * y
+                return unitCounter 
+        
+        return unitCounter
+            
+        
