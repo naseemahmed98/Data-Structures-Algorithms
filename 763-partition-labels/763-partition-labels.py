79d3e6390d501de7742cfd4fb174abb4bac404a2
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-07-24 17:29:35 -0400

    Time: 29 ms (84.88%), Space: 13.5 MB (29.30%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,37 +1,19 @@
 class Solution(object):
     def partitionLabels(self, s):
-        letter_freq = {}
-        partition_freq = {}
-        unique_letters = set()
-        
-        l, r = 0, 0  
+        last_index = {}
         res = []
-        for x in s:
-            if x not in letter_freq:
-                letter_freq[x] = 1 
-            else:
-                letter_freq[x] += 1 
-        print(letter_freq)
-        unique_letters.add(s[r])
-        print(unique_letters)
-        while r < len(s):
-         #   print(unique_letters)
+        for x, y in enumerate(s):
+            last_index[y] = x 
+        
+        size, end = 0, 0 
+        for x, y in enumerate(s):
+            size += 1 
+            end = max(end,last_index[y])
+            if x == end:
+                res.append(size)
+                size = 0 
+        return res
             
-            if s[r] not in partition_freq:
-                partition_freq[s[r]] = 1
-                #if letter_freq[s[r]] != 1:
-                unique_letters.add(s[r])
-            else:
-                partition_freq[s[r]] += 1 
-            if partition_freq[s[r]] == letter_freq[s[r]]:
-                    unique_letters.remove(s[r])
-            if not unique_letters:
-                res.append(r-l+1)
-                l = r+1
-          #  print(unique_letters)
-            r += 1
-        #print('this',partition_freq)
-      
-        return res            
+            
             
         
