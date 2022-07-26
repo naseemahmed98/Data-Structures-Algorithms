fb23599afde1cd6c67a5ea230660fb3006b61eb7
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-07-26 18:57:24 -0400

    Time: 326 ms (75.06%), Space: 32.4 MB (24.97%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,28 +1,29 @@
 class Solution(object): #DFS
     def numIslands(self, grid):
-        res = 0 
-        rows = len(grid)
-        cols = len(grid[0])
-
-        def find_island(x,y):
-            grid[x][y] = '0'
-            
-            if x + 1 < rows and grid[x+1][y] == '1': 
-                find_island(x+1,y)
-            
-            if x - 1 >= 0 and grid[x-1][y] == '1':
-                find_island(x-1,y)
-            
-            if y + 1 < cols and grid[x][y+1] == '1': 
-                find_island(x,y+1)
-            
-            if y - 1 >= 0 and grid[x][y-1] == '1':
-                find_island(x,y-1)
+        visited = set()
+        queue = []
+        rows, cols = len(grid), len(grid[0])
+        islands = 0 
         
-        for x in range(rows):
-            for y in range(cols):
-                if grid[x][y] == '1':
-                    res+=1
-                    find_island(x,y)
-                    #res += 1 
-        return res
+        def bfs(r,c):
+            queue.append([r,c])
+            while queue:
+                r,c = queue.pop(0)
+                if (r,c) not in visited:
+                    visited.add((r,c))
+                    if r - 1 >= 0 and grid[r-1][c] == '1' and [r-1,c] not in queue:
+                        queue.append([r-1,c])
+                    if r + 1 < rows and grid[r+1][c] == '1' and [r+1,c] not in queue :
+                        queue.append([r+1,c])
+                    if c - 1 >= 0 and grid[r][c-1] == '1'and [r,c-1] not in queue:
+                        queue.append([r,c-1])
+                    if c+1 < cols and grid[r][c+1] == '1' and [r,c+1] not in queue:
+                        queue.append([r,c+1])
+            
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r,c) not in visited:
+                    bfs(r,c)
+                    islands += 1 
+        return islands
+
