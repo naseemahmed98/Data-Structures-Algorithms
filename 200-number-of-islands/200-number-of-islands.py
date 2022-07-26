d1971c6e77aa8e90ea33995260613ce7fdf43b99
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-07-26 18:28:01 -0400

    Time: 232 ms (98.39%), Space: 28.6 MB (86.18%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,27 +1,27 @@
-class Solution:
+class Solution(object): #DFS
     def numIslands(self, grid):
-        visited = set()
-        row,col=len(grid),len(grid[0])
+        res = 0 
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def find_island(x,y):
+            grid[x][y] = '0'
+            
+            if x + 1 < rows and grid[x+1][y] == '1': 
+                find_island(x+1,y)
+            
+            if x - 1 >= 0 and grid[x-1][y] == '1':
+                find_island(x-1,y)
+            
+            if y + 1 < cols and grid[x][y+1] == '1': 
+                find_island(x,y+1)
+            
+            if y - 1 >= 0 and grid[x][y-1] == '1':
+                find_island(x,y-1)
         
-        qu = []
-        def bfs(r,c):
-            qu.append([r,c])
-            while qu:
-                r,c=qu.pop(0)
-                if (r,c) not in visited and grid[r][c]=='1':
-                    visited.add((r,c))
-                    if r!=0 and grid[r-1][c]=='1':#up
-                        qu.append([r-1,c])
-                    if r!=row-1 and grid[r+1][c]=='1':#down
-                        qu.append([r+1,c])
-                    if c!=0 and grid[r][c-1]=='1': #left
-                        qu.append([r,c-1])
-                    if c!=col-1 and grid[r][c+1]=='1':#right
-                        qu.append([r,c+1])
-        count=0
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c]=='1' and (r,c) not in visited:
-                    bfs(r,c)
-                    count+=1
-        return count
+        for x in range(rows):
+            for y in range(cols):
+                if grid[x][y] == '1':
+                    find_island(x,y)
+                    res += 1 
+        return res
