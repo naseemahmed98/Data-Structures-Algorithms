c3a0b8a277a7ba6994e3cb4eb17f318af969e6eb
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-07-27 01:22:12 -0400

    Time: 353 ms (68.29%), Space: 32.6 MB (17.98%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,29 +1,32 @@
-class Solution(object): #DFS
+from collections import deque
+class Solution(object): #BFS
     def numIslands(self, grid):
         visited = set()
-        queue = []
+        queue = deque()
         rows, cols = len(grid), len(grid[0])
-        islands = 0 
+        num_islands = 0 
         
-        def bfs(r,c):
-            queue.append([r,c])
+        def bfs(x,y):
+            queue.append((x,y))
             while queue:
-                r,c = queue.pop(0)
-                if (r,c) not in visited:
-                    visited.add((r,c))
-                    if r - 1 >= 0 and grid[r-1][c] == '1':
-                        queue.append([r-1,c])
-                    if r + 1 < rows and grid[r+1][c] == '1':
-                        queue.append([r+1,c])
-                    if c - 1 >= 0 and grid[r][c-1] == '1':
-                        queue.append([r,c-1])
-                    if c+1 < cols and grid[r][c+1] == '1':
-                        queue.append([r,c+1])
-            
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1' and (r,c) not in visited:
-                    bfs(r,c)
-                    islands += 1 
-        return islands
+                x,y = queue.popleft()
+                if (x,y) not in visited:
+                    visited.add((x,y))
+                    if x+1 < rows and grid[x+1][y] == '1':
+                        queue.append((x+1,y))
+                    if x-1 >= 0 and grid[x-1][y] == '1':
+                        queue.append((x-1,y))
+                    if y+1 < cols and grid[x][y+1] == '1':
+                        queue.append((x,y+1))
+                    if y-1 >= 0 and grid[x][y-1] == '1':
+                        queue.append((x,y-1))
+        
+        
+        
+        for x in range(rows):
+            for y in range(cols):
+                if grid[x][y] == '1' and (x,y) not in visited:
+                    num_islands += 1 
+                    bfs(x,y)
 
+        return num_islands
