9a2cc904e1bf34aa9855cbbca8f0d0aa10e407a7
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-07-30 15:42:24 -0400

    Time: 378 ms (47.58%), Space: 16.4 MB (81.45%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -2,22 +2,22 @@ class Solution(object):
     def numPairsDivisibleBy60(self, time):
         remainder_map = {}
         num_pairs = 0 
-        for x in time:
-            remainder_val = x % 60 
+        
+        for x in time: 
+            remainder_val = x % 60
+            
             if remainder_val == 0 and 0 in remainder_map:
                 num_pairs += remainder_map[0]
+            
             if (60-remainder_val) in remainder_map:
-                num_pairs += remainder_map[60-remainder_val] 
-                
-       
+                num_pairs += remainder_map[60-remainder_val]
+            
             if remainder_val not in remainder_map:
                 remainder_map[remainder_val] = 1 
             else:
-                remainder_map[remainder_val] += 1
-                
+                remainder_map[remainder_val] += 1 
             
-        print(remainder_map)
+            
+        
         return num_pairs
-     
-      
         
