f1243da2dab16f211d13191ae9d51c8452ee4be1
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-07-31 22:57:55 -0400

    Time: 24 ms (90.03%), Space: 13.6 MB (60.65%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -1,24 +1,24 @@
 class Solution:
     def mostCommonWord(self, paragraph, banned):
-        paragraph = paragraph.lower()
         banned_set = set(banned)
-        for x in "!?',;.":
-            paragraph = paragraph.replace(x," ")
         
-        word_freq={}
+        paragraph = paragraph.lower()
+        for x in "!?',;.'":
+            paragraph = paragraph.replace(x," ")
+            
+        word_freq = {}
+        counter = 0 
         max_word = ""
-        counter = 0
+        
         
         for x in paragraph.split():
-            if x not in banned_set:
+            if x not in banned:
                 if x not in word_freq:
                     word_freq[x] = 1 
                 else:
-                    word_freq[x] += 1
-                    
+                    word_freq[x] += 1 
                 if word_freq[x] > counter:
                     counter = word_freq[x]
                     max_word = x
-        
-   
         return max_word
+        
