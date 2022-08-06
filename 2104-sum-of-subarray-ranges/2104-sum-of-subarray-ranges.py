91c2b1ae94caa1fc987680c5093f8a083349b81b
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-08-05 23:14:27 -0400

    Time: 180 ms (61.91%), Space: 14.2 MB (45.55%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -3,7 +3,6 @@ class Solution:
         minsum = maxsum = 0
         stack = []
         for x in range(len(nums)+1):
-			
             while stack and (x == len(nums) or nums[stack[-1]] > nums[x]):
                 i = stack.pop()
                 if stack:
@@ -12,16 +11,15 @@ class Solution:
                     prev_smaller = -1
                 minsum += nums[i] * (x - i) * (i - prev_smaller)
             stack.append(x)
-     
         stack = []
-        for next_larger in range(len(nums) + 1):
-            while stack and (next_larger == len(nums) or nums[stack[-1]] < nums[next_larger]):
+        for x in range(len(nums) + 1):
+            while stack and (x == len(nums) or nums[stack[-1]] < nums[x]):
                 i = stack.pop()
                 if stack:
                     prev_larger = stack[-1]  
                 else:
                     prev_larger = -1
-                maxsum += nums[i] * (next_larger - i) * (i - prev_larger)
-            stack.append(next_larger)
+                maxsum += nums[i] * (x - i) * (i - prev_larger)
+            stack.append(x)
         
         return maxsum - minsum
