7fcb3047dd6368efd9e10057b8441c16941c54cd
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-08-07 15:14:38 -0400

    Time: 2635 ms (5.71%), Space: 25.5 MB (93.57%) - LeetHub

---
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minimumHealth(self, damage, armor):
+        total_health = 1
+        damage.sort(reverse = True)
+        armor_state = True 
+        # armor = 4... damage = [7,4,3,2]
+        
+        for x in damage:
+            print(x, armor, total_health)
+            if armor_state:
+                if x >= armor:
+                    total_health += (x - armor)
+                armor_state = False
+                continue
+            
+            total_health += x
+            
+        return total_health
+                
+            
