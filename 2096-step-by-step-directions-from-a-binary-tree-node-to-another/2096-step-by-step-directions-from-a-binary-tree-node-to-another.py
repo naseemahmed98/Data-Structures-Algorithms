10854821cef63ff3868dd63041b65c4caf4a858f
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-08-15 00:50:45 -0400

    Time: 1217 ms (86.99%), Space: 154.7 MB (38.38%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -1,25 +1,24 @@
 class Solution(object):
     def getDirections(self, root, startValue, destValue):
-        left_path = deque()
-        right_path = deque()
-        def helper(node, path, targetnode):
+        startPath, destPath = deque(), deque()
+        
+        def findTarget(node,val,path):
             if not node:
                 return 
-            if node.val == targetnode:
+            if node.val == val:
                 return True 
-            if helper(node.left, path, targetnode):
-                    path.appendleft("L")
-                    return True
-            if helper(node.right, path, targetnode):
+            if findTarget(node.left,val,path):
+                path.appendleft("L")
+                return True 
+            if findTarget(node.right,val,path):
                 path.appendleft("R")
                 return True 
-        helper(root, left_path,  startValue)
-        helper(root, right_path,  destValue)
-        while left_path and right_path and left_path[0] == right_path[0]:
-            left_path.popleft()
-            right_path.popleft()
-        return "U"*len(left_path) + ''.join(right_path)
         
+        findTarget(root,startValue, startPath)
+        findTarget(root,destValue, destPath)
         
+        while startPath and destPath and startPath[0] == destPath[0]:
+            startPath.popleft()
+            destPath.popleft()
         
-        
+        return "U" * len(startPath) + "".join(destPath)
