5cb92f637442853b82a0edc18867e0ca36a7daec
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-08-14 22:36:32 -0400

    Time: 871 ms (99.36%), Space: 154.9 MB (32.20%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -3,16 +3,16 @@ class Solution(object):
         left_path = deque()
         right_path = deque()
         def helper(node, path, targetnode):
+            if not node:
+                return 
             if node.val == targetnode:
                 return True 
-            if node.left:
-                if helper(node.left, path, targetnode):
+            if helper(node.left, path, targetnode):
                     path.appendleft("L")
                     return True
-            if node.right:
-                if helper(node.right, path, targetnode):
-                    path.appendleft("R")
-                    return True 
+            if helper(node.right, path, targetnode):
+                path.appendleft("R")
+                return True 
         helper(root, left_path,  startValue)
         helper(root, right_path,  destValue)
         while left_path and right_path and left_path[0] == right_path[0]:
