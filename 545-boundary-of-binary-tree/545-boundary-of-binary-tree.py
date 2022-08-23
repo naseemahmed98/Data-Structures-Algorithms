2eeb4e0a541f611c09d0c97957c781cea6da85aa
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-08-23 17:14:41 -0400

    Time: 51 ms (48.13%), Space: 16.2 MB (77.10%) - LeetHub

--- 545-boundary-of-binary-tree/545-boundary-of-binary-tree.py
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.py
@@ -4,46 +4,39 @@ class Solution:
             return [root.val]
         
         
-        def dfs(node, arr, is_right):
-            
+        def traverseBoundary(node, boundaryPath, isLeft):
             if not node:
                 return 
             
-            if not node.left and not node.right:
-                return 
-            
-            arr.append(node.val)
-            if is_right:
-                if node.right:
-                    dfs(node.right, arr, is_right)
-
-                else:
-                    dfs(node.left, arr, is_right)
-            else:
+            if isLeft:
                 if node.left:
-                    dfs(node.left, arr, is_right)
-                else:
-                    dfs(node.right, arr, is_right)
-                    
-        def find_leaf(node):
+                    boundaryPath.append(node.val)
+                    traverseBoundary(node.left,boundaryPath,isLeft)
+                elif node.right:
+                    boundaryPath.append(node.val)
+                    traverseBoundary(node.right,boundaryPath,isLeft)
+            
+            else:
+                if node.right:
+                    boundaryPath.append(node.val)
+                    traverseBoundary(node.right,boundaryPath,isLeft)
+                elif node.left:
+                    boundaryPath.append(node.val)
+                    traverseBoundary(node.left,boundaryPath,isLeft)
+        
+        def getLeaves(node):
             if not node.left and not node.right:
                 leaves.append(node.val)
-                return
             if node.left:
-                find_leaf(node.left)
-                
+                getLeaves(node.left)
             if node.right:
-                find_leaf(node.right)
-                
-                
+                getLeaves(node.right)
         
         
-        right = []
-        dfs(root.right, right, True)
-        left = []
-        dfs(root.left, left, False)
-        leaves = []
-        find_leaf(root)
-            
+        left,right,leaves = [root.val],[],[]
+        traverseBoundary(root.left,left,True)
+        traverseBoundary(root.right,right,False)
+        getLeaves(root)
+        return left + leaves + list(reversed(right))
         
-        return [root.val] + left + leaves + list(reversed(right))
+                    
