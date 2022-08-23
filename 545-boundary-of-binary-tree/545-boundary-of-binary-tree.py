80bb73c6fb808f370f6a07e52764bf5f9df21c6d
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-08-23 15:34:26 -0400

    Time: 62 ms (22.43%), Space: 16.6 MB (19.16%) - LeetHub

---
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.py
@@ -0,0 +1,52 @@
+class Solution:
+    def boundaryOfBinaryTree(self, root):
+        if not root.left and not root.right:
+            return [root.val]
+        
+        
+        def dfs(node, arr, is_right):
+            
+            if not node:
+                return 
+            
+            elif not node.left and not node.right:
+                return 
+            
+            arr.append(node.val)
+            if is_right:
+                if node.right:
+                    dfs(node.right, arr, is_right)
+
+                else:
+                    dfs(node.left, arr, is_right)
+            else:
+                if node.left:
+                    dfs(node.left, arr, is_right)
+                else:
+                    dfs(node.right, arr, is_right)
+                    
+        def find_leaf(node):
+            if not node.left and not node.right:
+                leaves.append(node.val)
+                return
+            if node.left:
+                find_leaf(node.left)
+                
+            if node.right:
+                find_leaf(node.right)
+                
+                
+        
+        
+        right = []
+        dfs(root.right, right, True)
+        left = []
+        dfs(root.left, left, False)
+        leaves = []
+        find_leaf(root)
+            
+        print(right)
+        print(left)
+        print(leaves)
+        
+        return [root.val] + left + leaves + list(reversed(right))
