814ac4142e8fc939844f9980361e705508b761dd
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-08-24 20:16:45 -0400

    Time: 73 ms (95.99%), Space: 14.9 MB (88.63%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,26 +1,29 @@
-class Solution(object):
+class Solution(object): #BFS
     def canFinish(self, numCourses, prerequisites):
+        num_pre = [0] * numCourses 
+        prereq_list = []
+        for x in range(numCourses):
+            prereq_list.append([])
+        no_prereqs = []
         
-        preMap = collections.defaultdict(list)
-        for crs, pre in prerequisites:
-            preMap[pre].append(crs)
-        visitedSet = set()
+        for b,a in prerequisites:
+            num_pre[b] += 1 
+            prereq_list[a].append(b)
         
-        print(preMap)
-        def dfs(crs):
-            if crs in visitedSet:
-                return False
-            if len(preMap[crs]) == 0:
-                return True
-            visitedSet.add(crs)
-            for pre in preMap[crs]:
-                if not dfs(pre):
-                    return False
-            visitedSet.remove(crs)
-            preMap[crs] = []
+        for x in range(numCourses):
+            if num_pre[x] == 0:
+                no_prereqs.append(x)
+        
+        counter = 0 
+        while no_prereqs:
+            course = no_prereqs.pop(0)
+            counter += 1 
+            for x in prereq_list[course]:
+                num_pre[x] -= 1 
+                if num_pre[x] == 0:
+                    no_prereqs.append(x)
+                    
+        if counter == numCourses:
             return True 
-        
-        for crs in range(numCourses):
-            if not dfs(crs):
-                return False
-        return True
+        else:
+            return False
