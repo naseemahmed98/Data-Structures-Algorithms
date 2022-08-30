16e7f779da338b5f9869fab0d5b1ec58288195a0
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-08-29 20:30:45 -0400

    Time: 388 ms (71.10%), Space: 15 MB (74.57%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.py
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -7,19 +7,16 @@ class Solution:
         for x,y in roads:
             hashmap[x].append(y)
             hashmap[y].append(x)
-        print(hashmap)
+    
         final=[]
         maxlen=0
         for i in range(n):
             for j in range(i+1,n):
+                a=hashmap[i]
+                b=hashmap[j]
+                final=a+b
                 if j in hashmap[i]:
-                    a=hashmap[i]
-                    b=hashmap[j]
-                    final=a+b
                     maxlen=max(maxlen,len(final)-1)
                 else:
-                    a=hashmap[i]
-                    b=hashmap[j]
-                    final=a+b
                     maxlen=max(maxlen,len(final))
         return maxlen
