07b044160bc70ee136f9a1cb935933c0c083210e
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-08-30 18:54:56 -0400

    Time: 1467 ms (6.32%), Space: 21.2 MB (14.94%) - LeetHub

---
+++ 886-possible-bipartition/886-possible-bipartition.py
@@ -0,0 +1,23 @@
+class Solution:
+    def possibleBipartition(self, N, dislikes):
+        graph = collections.defaultdict(list)
+        for u,v in dislikes:
+            graph[u].append(v)
+            graph[v].append(u)
+            
+            
+        def dfs(node, col):
+            color[node] = col
+            for neigh in graph[node]:
+                if color[neigh] == col:
+                    return False
+                if color[neigh] == 0 and not dfs(neigh, -col):
+                    return False
+            return True 
+        
+        
+        color = [0] * (N+1)  
+        for i in range(1,N):
+            if color[i] == 0 and not dfs(i, 1):
+                return False
+        return True
