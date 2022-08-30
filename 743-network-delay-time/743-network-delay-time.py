040b2482323560c3a9026d81242f1e9252b488a9
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-08-30 01:43:36 -0400

    Time: 721 ms (31.42%), Space: 15.8 MB (58.51%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def networkDelayTime(self, times, n, k):
+        
+        networkMapping = collections.defaultdict(list)
+        
+        for x,y,z in times:
+            networkMapping[x].append((y,z)) 
+
+        print(networkMapping)
+        signalTimes = {k:0}
+        queue = deque([(k,0)])
+        
+        
+        while queue:
+            node,time = queue.popleft()
+            for nextNode, nextNodeTime in networkMapping[node]:
+                if nextNode == k: 
+                    continue
+                if (nextNode not in signalTimes) or (time + nextNodeTime < signalTimes[nextNode]):
+                    signalTimes[nextNode] = time + nextNodeTime
+                    queue.append((nextNode,signalTimes[nextNode]))
+       
+        return max(list(signalTimes.values())) if len(signalTimes) == n else -1
