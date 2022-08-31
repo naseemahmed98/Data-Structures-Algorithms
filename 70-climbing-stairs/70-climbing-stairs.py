ab1d05e963bca9776f0dfc701f40b69fa7d6d851
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-08-30 21:38:57 -0400

    Time: 17 ms (86.26%), Space: 13.6 MB (12.60%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,14 +1,22 @@
+
+
+    
 class Solution(object):
     def climbStairs(self, n):
         def topDown(step):
             if step == 0 or step == 1:
-                return 1 
+                return 1
             if step in dp:
                 return dp[step]
+
             dp[step] = topDown(step-1) + topDown(step-2)
+            
             return dp[step]
         dp = {}
         return topDown(n)
+    
+    
+    
 
     
     
