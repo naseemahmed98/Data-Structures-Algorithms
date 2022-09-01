c081e7e53ee7fdc5e77f4a689277a8b4d1bf138c
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-01 18:32:03 -0400

    Time: 862 ms (21.47%), Space: 23.9 MB (13.68%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,13 +1,15 @@
-class Solution(object):
-    def longestCommonSubsequence(self, text1, text2):
-        dp = [ [0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
-        
-        for x in range(1,len(text1)+1):
-            for y in range(1,len(text2)+1):
-                if text1[x-1] == text2[y-1]:
-                    dp[x][y] = 1 + dp[x-1][y-1]
-                else:
-                    dp[x][y] = max(dp[x-1][y],dp[x][y-1])
-        
-        print(dp)
-        return dp[-1][-1]
+class Solution:
+    def longestCommonSubsequence(self, text1,text2):
+        def recurMe(i,j):
+            if i<0 or j<0:
+                return 0
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            if text1[i]==text2[j]:
+                dp[i][j]=1+recurMe(i-1,j-1)
+                return dp[i][j]
+            else:
+                dp[i][j]=max(recurMe(i-1,j),recurMe(i,j-1))
+                return dp[i][j]
+        dp=[[-1 for j in range(len(text2))] for i in range(len(text1))]
+        return recurMe(len(text1)-1,len(text2)-1)
