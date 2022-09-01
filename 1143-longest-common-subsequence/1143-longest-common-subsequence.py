43b78cc7ef593ed959660aa4e3eb9fc7818db0f5
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-01 19:59:12 -0400

    Time: 997 ms (19.06%), Space: 24.3 MB (11.84%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,18 +1,17 @@
 class Solution:
     def longestCommonSubsequence(self, text1,text2):
-        def recurMe(i,j):
-            if i == len(text1) or j == len(text2):
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if text1[i]==text2[j]:
-                dp[i][j]=1+recurMe(i+1,j+1)
-                return dp[i][j]
+        def topDown(x,y):
+            if x == len(text1) or y == len(text2):
+                return 0 
+            if dp[x][y] != 0:
+                return dp[x][y]
+            if text1[x] == text2[y]:
+                dp[x][y] = 1 + topDown(x+1,y+1)
+                return dp[x][y]
             else:
-                dp[i][j]=max(recurMe(i+1,j),recurMe(i,j+1))
-                return dp[i][j]
-        dp=[[-1 for j in range(len(text2))] for i in range(len(text1))]
+                dp[x][y] = max(topDown(x+1,y),topDown(x,y+1))
+                return dp[x][y]
         
-        a = recurMe(0,0)
-        print(dp)
-        return a
+        dp = [[0 for y in range(len(text2))] for x in range(len(text1))]
+        return topDown(0,0)
+        
