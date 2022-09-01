7319362143e1e49a46acea33a87bd9af1eee25f3
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-01 02:18:00 -0400

    Time: 29 ms (80.06%), Space: 13.7 MB (17.34%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,9 +1,15 @@
 class Solution(object):
     def combinationSum4(self, nums, target):
-        dp = [0] * (target + 1)
-        dp[0] = 1
-        for a in range(1,target + 1):
-            for c in nums:
-                if a >= c:
-                    dp[a] = dp[a] + dp[a - c]
-        return dp[target]
+        memoization = {}
+        def dp(amount):
+            if amount == 0:
+                return 1
+            if amount in memoization:
+                return memoization[amount]
+            memoization[amount] = 0 
+            for x in nums:
+                if amount >= x:
+                    memoization[amount] = memoization[amount] + dp(amount-x)
+            return memoization[amount]
+        
+        return dp(target)
