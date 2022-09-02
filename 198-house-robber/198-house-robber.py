d6be17cae30f859d67957cfe6a2302283061d8a7
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-02 17:13:51 -0400

    Time: 35 ms (24.76%), Space: 13.2 MB (89.23%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -2,17 +2,12 @@ class Solution(object):
     def rob(self, nums):
         if len(nums) <= 2:
             return max(nums)
+        dp = [0] * len(nums)
+        nums[1] = max(nums[0],nums[1])
         
-        memoization = {}
-        def topDown(x):
-            if x == 0:
-                return nums[0]
-            if x == 1:
-                return max(nums[0],nums[1])
-            if x in memoization:
-                return memoization[x]
-            memoization[x] = max(nums[x]+topDown(x-2),topDown(x-1))
-            return memoization[x]
+        #dp = [2,7,0,0,0]
         
-        return topDown(len(nums)-1)
-        
+        for x in range(2,len(dp)):
+            nums[x] = max(nums[x]+nums[x-2],nums[x-1])
+        return nums[-1]
+    
