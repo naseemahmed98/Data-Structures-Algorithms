f582330decb5f2f62433316092b72f0531e3c783
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-01 21:19:30 -0400

    Time: 34 ms (26.69%), Space: 13.2 MB (89.28%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,26 +1,17 @@
 class Solution(object):
     def rob(self, nums):
-        dct = {}
-        if len(nums) <= 3:
-            return max(nums)
-        def calculate_max_sum(nums,x,dct):
-            #print(nums,x,dct)
-            if x == 1:
-                return max(nums[0],nums[1])
-            if x == 0:
-                return nums[0]
-            if x in dct:
-                return dct[x]
-            max_sum = max(calculate_max_sum(nums,x-1,dct), calculate_max_sum(nums,x-2,dct) + nums[x])
-            dct[x] = max_sum
-            return max_sum
-        
     
-        shorter_length = len(nums)-1 
+        if len(nums) <=2:
+            return max(nums)
         
-        #print(len(nums),nums[:len(nums)-1], nums[1:len(nums)])
+        def houseRobber(array):
+            dp = [0] * len(array)
+            dp[0],dp[1] = array[0], max(array[0],array[1])
+            for x in range(2,len(array)):
+                dp[x] = max(array[x]+dp[x-2],dp[x-1])
+            return dp[-1]
         
-        first_house = calculate_max_sum(nums[:shorter_length], shorter_length-1, {})
-        second_house = calculate_max_sum(nums[1:], shorter_length-1,{})
         
-        return max(first_house, second_house)
+        
+        return max(houseRobber(nums[:-1]),houseRobber(nums[1:]))
+        
