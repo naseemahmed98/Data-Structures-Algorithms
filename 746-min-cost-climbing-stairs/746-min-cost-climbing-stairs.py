4419510b0c66a716df0cbb3410af77789513a78c
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-02 02:36:53 -0400

    Time: 70 ms (40.07%), Space: 13.5 MB (76.44%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,6 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, cost):
-        cost.append(0)
-        for x in range(len(cost)-3,-1,-1):
-            cost[x] = min(cost[x+1]+cost[x], cost[x+2]+cost[x])
-        return min(cost[0],cost[1])
+        cost = deque(cost)
+        cost.appendleft(0)
+        print(cost)
+        for x in range(1,len(cost)):
+            cost[x] = min(cost[x-1]+cost[x], cost[x-2]+cost[x])
+        return min(cost[-1],cost[-2])
