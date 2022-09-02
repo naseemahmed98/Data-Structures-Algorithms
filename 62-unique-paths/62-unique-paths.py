6c15233f9f0797702331f3d9d4d608fbd9b73074
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-02 18:24:57 -0400

    Time: 31 ms (39.63%), Space: 13.8 MB (6.47%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def uniquePaths(self, m, n):
-        dp = [[0 for x in range(n)] for y in range(m)]
+        
         
         memoization = {}
         def topDown(x,y):
