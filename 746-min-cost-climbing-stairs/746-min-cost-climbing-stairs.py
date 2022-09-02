e1cc5371960ad665b54cf4d533a1fdc468ebef38
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-02 17:25:43 -0400

    Time: 61 ms (57.57%), Space: 15.8 MB (6.47%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,10 +1,13 @@
 class Solution:
     def minCostClimbingStairs(self, cost):
-        for x in range(2,len(cost)):
-            if x == len(cost)-1:
-                cost[x] = cost[x] + cost[x-2]
-            else:
-                cost[x] = min(cost[x-1],cost[x-2])+ cost[x]
-        return min(cost[-1],cost[-2])
+        memoization = {}
         
+        def topDown(x):
+            if x == 0 or x == 1:
+                return cost[x]
+            if x in memoization:
+                return memoization[x]
+            memoization[x] = min(topDown(x-1),topDown(x-2)) + cost[x]
+            return memoization[x]
         
+        return min(topDown(len(cost)-1),topDown(len(cost)-2))
