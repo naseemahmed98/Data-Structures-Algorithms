87049a7052ef77b40084b3b38c784bd1018c0837
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-05 21:29:11 -0400

    Time: 55 ms (99.40%), Space: 15.3 MB (5.05%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,23 +1,18 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-      
-        res = []
-        
-        def dfs(i,curr,total):
-        
-            if total == target:
-                res.append(curr.copy())
-                return
-            if i == len(candidates) or total > target:
-                return 
-            
-            curr.append(candidates[i])
-            dfs(i, curr, total + candidates[i])
-            curr.pop()
-            dfs(i+1,curr,total)
-        
-        dfs(0,[],0)
-        return res
+    def combinationSum(self, candidates : List[int], target: int) -> List[List[int]]:    
+        dp = [[] for i in range(target+1)]
+        for x in candidates :
+            if x <= target:
+                dp[x].append([x])  # x is the sum, this cover the only one element case.
+
+            #print(x, x+1, target+1)
+            for the_sum in range(x+1, target+1):# x+1 to target(include)
+                for lst in dp[the_sum-x]:
+                    dp[the_sum].append(lst+[x])
+                    
+            #for row in dp: print(row)
+            #print("-"*20)
+        return dp[-1]
                 
         
         
