873d1391589efb44e259ff536937ae8d32f8fb26
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-05 22:08:39 -0400

    Time: 104 ms (74.75%), Space: 15.3 MB (6.26%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -3,15 +3,11 @@ class Solution:
         dp = [[] for i in range(target+1)]
         for x in candidates :
             if x <= target:
-                dp[x].append([x])  # x is the sum, this cover the only one element case.
-
-            #print(x, x+1, target+1)
-            for the_sum in range(x+1, target+1):# x+1 to target(include)
-                for lst in dp[the_sum-x]:
-                    dp[the_sum].append(lst+[x])
-                    
-            #for row in dp: print(row)
-            #print("-"*20)
+                dp[x].append([x]) 
+            for y in range(x, target+1):# x+1 to target(include)
+                for lst in dp[y-x]:
+                    dp[y].append(lst+[x])
+    
         return dp[-1]
                 
         
