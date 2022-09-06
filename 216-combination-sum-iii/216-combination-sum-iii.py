f413d8af1a7d0a310621a5a0c210f91f464d58ed
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-06 03:18:10 -0400

    Time: 42 ms (6.05%), Space: 13.5 MB (39.55%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -1,7 +1,9 @@
 class Solution(object):
     def combinationSum3(self, k, n):
         dp = [set() for x in range(n + 1)]
+        print(dp)
         dp[0].add(())
+        print(dp)
         characters = [i for i in range(1, 10)]
 
         for char_num in characters:
@@ -10,7 +12,7 @@ class Solution(object):
                     current_val = prev + (char_num,)
                     dp[num_range].add(current_val)
         res = []
-        print(dp)
+        
         for each in dp[-1]:
             if len(each) == k:
                 res.append(each)
