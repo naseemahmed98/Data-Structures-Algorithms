7bc268224ccaec08feb090e82652658c53049344
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-06 01:59:54 -0400

    Time: 1723 ms (5.04%), Space: 157.1 MB (5.79%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        dp = [[] for x in range(n+1)]
+        for x in range(n):
+            if x > 9:
+                break
+            dp[x].append([x])
+            for y in range(x+1,n+1):
+                for z in dp[y-x]:
+                    dp[y].append(z + [x])
+        for x in dp[-1]:
+            if len(x) == k and len(set(x)) == len(x):
+                res.append(x)
+        return res
+                        
