624ff81f5ff492cb1c8861937a1a98ef6703a412
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-07 07:33:04 -0400

    Time: 1049 ms (21.16%), Space: 36 MB (16.83%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.py
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -1,13 +1,22 @@
 class Solution(object):
     def change(self, amount, coins):
-        dp = [0] * (amount+1)
-        dp[0] = 1 
+    
         
-        for x in coins:
-            if x <= amount:
-                dp[x] += 1 
-            for y in range(x+1,amount+1):
-                dp[y] += dp[y-x]
-        
-        return dp[-1]
-      
+        def dp(coin,amount):
+            if amount == 0:
+                return 1 
+            
+            if coin == len(coins):
+                return 0
+            if memo[coin][amount] != -1:
+                return memo[coin][amount]
+            
+            take = 0 
+            if coins[coin] <= amount:
+                take = dp(coin,amount-coins[coin])
+            notTake = dp(coin+1,amount)
+            memo[coin][amount] = take + notTake
+            return take + notTake
+    
+        memo = [[-1 for x in range(amount+1)] for y in range(len(coins))]
+        return dp(0,amount)
