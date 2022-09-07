cbaec64ab2436632a7b18abc2f9a8720b61143de
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-07 07:25:05 -0400

    Time: 175 ms (16.53%), Space: 13.8 MB (23.10%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,14 +1,18 @@
 class Solution(object):
     def combinationSum(self, candidates, target):
-        dp = [[] for x in range(target+1)]
+        res= []
 
-        for x in candidates:
-            if x <= target:
-                dp[x].append([x])
-            for y in range(x+1,target+1):
-                for z in dp[y-x]:
-                    dp[y].append(z + [x])
         
-        return dp[-1]
-                
+        def backTrack(i,curr,total):
+            if total == target:
+                res.append(curr[:])
+                return
+            if i == len(candidates) or total > target:
+                return
+            curr.append(candidates[i])
+            backTrack(i,curr,total+candidates[i])
+            curr.pop()
+            backTrack(i+1,curr,total)
         
+        backTrack(0,[],0)
+        return res
