2cfbc63a790fa9dbfbb6d812a410128d4abcdb09
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-07 07:51:42 -0400

    Time: 41 ms (8.02%), Space: 13 MB (99.75%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -1,15 +1,21 @@
 class Solution(object):
     def combinationSum3(self, k, n):
-        dp = [[] for x in range(n+1)]
-        characters = list(range(1,10))
-        dp[0].append(())
-        
-        for x in characters:
-            for y in range(n,x-1,-1):
-                for z in dp[y-x]:
-                    dp[y].append(z+(x,))
+    
+        candidates = list(range(1,10))
         res = []
-        for x in (dp[-1]):
-            if len(x) == k:
-                res.append(x)
+    
+        def dfs(i,curr,total):
+            if total == n and len(curr) == k:
+                res.append(curr[:])
+                return
+            
+            if i >= len(candidates) or total > n:
+                return
+            
+            curr.append(candidates[i])
+            dfs(i+1,curr,total+candidates[i])
+            curr.pop()
+            dfs(i+1,curr,total)
+        
+        dfs(0,[],0)
         return res
