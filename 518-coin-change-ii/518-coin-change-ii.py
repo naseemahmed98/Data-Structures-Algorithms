64e8c1ed77347cf7b804e6bc79c6d25e51d43746
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-07 07:28:04 -0400

    Time: 175 ms (69.33%), Space: 13.6 MB (78.83%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.py
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -1,10 +1,13 @@
 class Solution(object):
     def change(self, amount, coins):
-        dp = [0] * (amount+ 1)
+        dp = [0] * (amount+1)
         dp[0] = 1 
         
-        for y in coins:
-            for x in range(1,amount+1):
-                if x - y >= 0:
-                    dp[x] += dp[x-y]
+        for x in coins:
+            if x <= amount:
+                dp[x] += 1 
+            for y in range(x+1,amount+1):
+                dp[y] += dp[y-x]
+        
         return dp[-1]
+      
