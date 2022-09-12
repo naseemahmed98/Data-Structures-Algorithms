e96f60b4f79a7a9c9862abe8834e9f728da7345f
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-12 08:14:44 -0400

    Time: 3709 ms (24.12%), Space: 57.4 MB (27.65%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.py
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def platesBetweenCandles(self, s, queries):
-        p = [i for i in range(len(s)) if s[i] == '|']
+        p = [x for x in range(len(s)) if s[x] == "|"]
         res = []
-        for x,y in queries: 
-            l = bisect.bisect_left(p, x)
-            r = bisect.bisect_right(p, y)
-            if r - l <=1:
+        for x, y in queries:
+            l = bisect.bisect_left(p,x)
+            r = bisect.bisect_right(p,y)
+            if r - l <= 1:
                 res.append(0)
             else:
-                
                 res.append(p[r-1] - p[l] + 1 - (r-l))
         return res
+        
