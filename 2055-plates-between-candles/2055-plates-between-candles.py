2f3f1a413e9a99f1ea9108d7ffcc930d77083bd8
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-13 05:34:35 -0400

    Time: 4119 ms (15.30%), Space: 57.4 MB (26.47%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.py
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -1,36 +1,19 @@
 class Solution(object):
     def platesBetweenCandles(self, s, queries):
-        numStars = [0] * len(s)
-        if s[0] == "*":
-            numStars[0] = 1 
-        
-        leftArray = [-1] * len(s)
-        if s[0] == "|":
-            leftArray[0] = 0 
-        
-        for x in range(1,len(s)):
-            if s[x] == "*":
-                numStars[x] = 1 + numStars[x-1]
-                leftArray[x] = leftArray[x-1]
-            else:
-                numStars[x] = numStars[x-1]
-                leftArray[x] = x 
-        
-        rightArray = [float("inf")] * len(s)
-        if s[-1] == "|":
-            rightArray[-1] = len(s)-1
-        for x in range(len(s)-2,-1,-1):
+        candles = []
+        for x in range(len(s)):
             if s[x] == "|":
-                rightArray[x] = x 
-            else:
-                rightArray[x] = rightArray[x+1]
-        
+                candles.append(x)
+        print(candles)
         res = []
         for x,y in queries:
-            leftIndex, rightIndex = rightArray[x], leftArray[y]
-            if rightIndex - leftIndex > 1:
-                res.append(numStars[rightIndex]-numStars[leftIndex])
-            else:
-                res.append(0)
-        return res            
+            leftIndex = bisect.bisect_left(candles,x)
+            rightIndex = bisect.bisect_right(candles,y)
             
+            if rightIndex - leftIndex <= 1:
+                res.append(0)
+            else:
+                res.append(candles[rightIndex-1] - candles[leftIndex] + 1 - (rightIndex-leftIndex))
+        return res
+            
+                
