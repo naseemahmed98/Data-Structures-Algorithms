680fac8976575e9e0b7ea3179839a7e9a356eae8
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-18 05:55:57 -0400

    Time: 1418 ms (37.12%), Space: 13.2 MB (91.80%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -1,30 +1,25 @@
 class Solution(object):
     def fourSum(self, nums, target):
+        nums.sort() 
         res = []
-        nums.sort()
-
-        for i in range(len(nums)):
-            
+        for i in range(len(nums)-3):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            for j in range(i+1, len(nums)):
-                #checking j duplicates
+            for j in range(i+1,len(nums)):
                 if j > i+1 and nums[j] == nums[j-1]:
-                    continue
-
-                l, r = j + 1, len(nums) - 1
-
+                    continue 
+            
+                l, r = j+1,len(nums)-1
                 while l < r:
-                    threeSum = nums[l] + nums[r] + nums[i] + nums[j]
-                    if threeSum > target:
-                        r -= 1
-                    elif threeSum < target:
-                        l += 1
-                    else:
-                        res.append([nums[l], nums[r], nums[i], nums[j]])
-                        l += 1
-                        #checking l duplicates
-                        while l < r and nums[l] == nums[l-1]:
-                            l += 1
+                    totalSum = nums[l] + nums[r] + nums[i] + nums[j]
+                    if totalSum > target:
+                        r -= 1 
+                    elif totalSum < target:
+                        l += 1 
 
+                    else:
+                        res.append([nums[l],nums[r],nums[i],nums[j]])
+                        l += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1 
         return res
