4b5557fc7794c9b205f699bba87c363c3274d475
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-20 22:50:41 -0400

    Time: 36 ms (79.72%), Space: 13.4 MB (40.21%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.py
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.py
@@ -1,20 +1,15 @@
 class Solution(object):
-    def twoSumLessThanK(self, movieTimes, duration):
-        indices = {}
-        for x in range(len(movieTimes)):
-            if movieTimes[x] not in indices:
-                indices[movieTimes[x]] = x
-        movieSum = duration 
-        movieTimes.sort() #60,75,85,90,120,125,150
-        l, r = 0,len(movieTimes)-1 
+    def twoSumLessThanK(self, nums, k):
+        nums.sort() #60,75,85,90,120,125,150
+        l, r = 0,len(nums)-1 
         largestPossibleSum = float("-inf")
         while l < r: 
-            if movieTimes[r] + movieTimes[l] >= movieSum:
+            mySum = nums[r] + nums[l]
+            if mySum >= k:
                 r -= 1 
-            if movieTimes[r] + movieTimes[l] < movieSum:
-                if movieTimes[r] + movieTimes[l] > largestPossibleSum:
-                    largestPossibleSum = movieTimes[l] + movieTimes[r]
-
+            if mySum < k:
+                if mySum > largestPossibleSum:
+                    largestPossibleSum = mySum
                 l += 1 
         return largestPossibleSum if largestPossibleSum > float("-inf") else -1 
         
