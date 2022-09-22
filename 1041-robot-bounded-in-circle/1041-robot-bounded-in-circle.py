c61cdb033f3fa40810d56a6f5b6b35cf2a4134c6
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-22 17:20:40 -0400

    Time: 24 ms (68.03%), Space: 13.2 MB (96.15%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -10,6 +10,6 @@ class Solution(object):
                 x_dir, y_dir = -1 * y_dir, x_dir
             if i == "R":
                 x_dir, y_dir = y_dir, -1 * x_dir
-        
-        return (x == 0 and y == 0) or (x_dir != 0 or y_dir != 1)
+            
+        return (x == 0 and y == 0) or x_dir != 0 or y_dir != 1
     
