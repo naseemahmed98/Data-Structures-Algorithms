52965e170c79b84998a426b422842f0e7cc2c1b1
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-27 04:43:22 -0400

    Time: 59 ms (24.74%), Space: 13.6 MB (48.11%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,12 +1,18 @@
 class Solution(object):
     def wordBreak(self, s, wordDict):
-        dp = [False] * (len(s) + 1 )
-        dp[len(s)] = True
-        for x in range(len(s)-1, -1, -1):
-            for z in wordDict:
-                if x + len(z) <= len(s):
-                    if s[x:x+len(z)] == z:
-                        dp[x] = dp[x+len(z)]
-                if dp[x]:
-                    break
-        return dp[0]
+        s_len = len(s)
+        
+        # initialize the DP talbe. 
+        dp = [False] * (s_len + 1)
+        # start from empty string as the seed. 
+        dp[0] = True
+        
+        # building the DP table.
+        for i in range(s_len):
+            if dp[i]:
+                for w in wordDict:
+                    if s[i:i + len(w)] == w and i + len(w) <= s_len:
+                        dp[i + len(w)] = True
+        
+        # our result is the dp[s_len]
+        return dp[s_len]
