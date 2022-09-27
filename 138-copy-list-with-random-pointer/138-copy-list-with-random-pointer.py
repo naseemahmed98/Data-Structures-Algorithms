3304f1109c9026065bd8b358bd55e3ec448c4a17
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-26 21:49:34 -0400

    Time: 85 ms (21.92%), Space: 14.3 MB (40.98%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -15,19 +15,22 @@ class Solution(object):
         cur = head
         m = {}
         m[head] = Node(head.val)
+        iteration = 0 
         
         while cur:
-            if cur not in m:
-                m[cur] = Node(cur.val)
+        
             
             if cur.next and cur.next not in m:
+                #print('dos',iteration)
                 m[cur.next] = Node(cur.next.val)
             
             if cur.random and cur.random not in m:
-                    m[cur.random] = Node(cur.random.val)
+                #print('tres',iteration)
+                m[cur.random] = Node(cur.random.val)
 
             m[cur].next = m[cur.next] if cur.next in m else None
             m[cur].random = m[cur.random] if cur.random in m else None
 			
             cur = cur.next
+            iteration += 1 
         return m[head]
