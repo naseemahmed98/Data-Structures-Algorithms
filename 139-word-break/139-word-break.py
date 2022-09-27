b52a07878e8443f74f2358b5a8295f754e668fc2
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-27 04:53:22 -0400

    Time: 29 ms (83.92%), Space: 13.7 MB (48.11%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -7,13 +7,13 @@ class Solution(object):
         # start from empty string as the seed. 
         dp[0] = True
         
-        # building the DP table.
+    
         for i in range(s_len):
             if dp[i]:
                 for w in wordDict:
-                    print(i)
-                    if i + len(w) <= s_len and s[i:i + len(w)] == w:
+                   
+                    if s[i:i + len(w)] == w and i + len(w):
                         dp[i + len(w)] = True
         
         # our result is the dp[s_len]
-        return dp[s_len]
+        return dp[-1]
