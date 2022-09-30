bead28a9ed5a511d5e91f63e3aed7f0d59146dc9
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-09-29 20:26:50 -0400

    Time: 178 ms (22.93%), Space: 14.6 MB (97.51%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,29 +1,27 @@
-class Solution(object):
+class Solution(object): #BFS
     def canFinish(self, numCourses, prerequisites):
+        num_pre = [0] * numCourses 
+        prereq_list = []
+        for x in range(numCourses):
+            prereq_list.append([])
+        no_prereqs = []
         
-        preReqs = collections.defaultdict(list)
-        for x,y in prerequisites:
-            preReqs[x].append(y)
-        
-        memo = {}
-        def dfs(course,visited):
-            if course in memo:
-                return memo[course]
-            if course in visited:
-                return False
-            if not preReqs[course]:
-                return True
-            visited.add(course)
-            for x in preReqs[course]:
-                if not dfs(x,visited):
-                    return False
-            
-            memo[course] = True
-            return True
-        
+        for b,a in prerequisites:
+            num_pre[b] += 1 
+            prereq_list[a].append(b)
         
         for x in range(numCourses):
-            if not dfs(x,set()):
-                print(x)
-                return False
-        return True
+            if num_pre[x] == 0:
+                no_prereqs.append(x)
+        
+        counter = 0 
+        while no_prereqs:
+            course = no_prereqs.pop(0)
+            counter += 1 
+            for x in prereq_list[course]:
+                num_pre[x] -= 1 
+                if num_pre[x] == 0:
+                    no_prereqs.append(x)
+                    
+        return True if counter == numCourses else False
+        
