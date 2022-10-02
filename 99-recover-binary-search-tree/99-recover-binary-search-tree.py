a4bbffc0b8758023500c2f6e67eb6c4a7c281d10
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-10-02 17:49:06 -0400

    Time: 148 ms (7.74%), Space: 14.1 MB (20.00%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -6,21 +6,19 @@
 #         self.right = right
 class Solution(object):
     def recoverTree(self, root):
-        self.previous_node = TreeNode(float("-inf"))
-        self.switch_one, self.switch_two = None, None 
+        self.prev = TreeNode(float("-inf"))
+        self.switchOne, self.switchTwo = None, None 
+        
         def inorder(node):
             if not node:
                 return 
             inorder(node.left)
-            if not self.switch_one and node.val < self.previous_node.val:
-                self.switch_one = self.previous_node
-            if node.val < self.previous_node.val:
-                self.switch_two = node
-            self.previous_node = node 
+            if not self.switchOne and node.val < self.prev.val:
+                self.switchOne = self.prev 
+            if node.val < self.prev.val:
+                self.switchTwo = node
+            self.prev = node
             inorder(node.right)
         
         inorder(root)
-        self.switch_one.val, self.switch_two.val = self.switch_two.val,self.switch_one.val
-        
-            
-        
+        self.switchOne.val,self.switchTwo.val = self.switchTwo.val, self.switchOne.val
