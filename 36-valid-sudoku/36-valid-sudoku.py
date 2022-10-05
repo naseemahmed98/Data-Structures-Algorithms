5d8a51888baa7a65095758906c85795c0fee7a36
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-10-04 22:23:21 -0400

    Time: 65 ms (98.85%), Space: 13.6 MB (19.13%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,23 +1,16 @@
 class Solution(object):
     def isValidSudoku(self, board):
-        rowContains = set()
-        colContains = set()
+        rowContains = defaultdict(set)
+        colContains = defaultdict(set)
         squares = defaultdict(set)
         for x in range(9):
             for y in range(9):
-                if board[x][y] != ".":
-                    if board[x][y] in rowContains or board[x][y] in squares[(x//3,y//3)]:
+                value = board[x][y]
+                if value != ".":
+                    if value in rowContains[x] or value in colContains[y] or value in squares[(x//3,y//3)]:
                         return False
                     else:
-                        rowContains.add(board[x][y])
-                        squares[(x//3,y//3)].add(board[x][y])
-            
-                if board[y][x] != ".":
-                    if board[y][x] in colContains:
-                        return False
-                    else:
-                        colContains.add(board[y][x])
-              
-                
-            rowContains, colContains = set(),set()
+                        rowContains[x].add(value)
+                        colContains[y].add(value)
+                        squares[(x//3,y//3)].add(value)
         return True
