69cf402e873399368e7c802a90fa8c67b6569230
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-10-05 03:35:17 -0400

    Time: 27 ms (99.17%), Space: 13.5 MB (13.87%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def intToRoman(self, num):
+        romanValues = [("I",1),("IV",4),("V",5),("IX",9),("X",10),("XL",40),("L",50),("XC",90),("C",100),("CD",400),("D",500),("CM",900),("M",1000)]
+        
+        lst = []
+        
+        for x in range(len(romanValues)-1,-1,-1):
+            roman, integer = romanValues[x]
+            if num // integer:
+                numSymbols = num // integer
+                lst.append(roman*numSymbols)
+                num = num % integer
+        
+        res = ''.join(lst)
+        return res
