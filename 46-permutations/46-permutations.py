324e8fafd1ca75ba6e0c9fd490a6f984ae614667
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-10-05 04:46:01 -0400

    Time: 28 ms (87.09%), Space: 13.6 MB (64.47%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def permute(self, nums):
+        def helper(ind,nums):
+            if ind>=len(nums):
+                res.append(nums[:])
+                return
+            for i in range(ind,len(nums)):
+                nums[ind],nums[i]=nums[i],nums[ind]
+                helper(ind+1,nums)
+                nums[ind],nums[i]=nums[i],nums[ind]
+        
+        res=[]
+        helper(0,nums)
+        return res
+        
