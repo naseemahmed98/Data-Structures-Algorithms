ac53349025f55b00e12d0e79a3632f731723c7ff
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-10-04 22:28:00 -0400

    Time: 167 ms (5.28%), Space: 16.9 MB (42.64%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.py
+++ 419-battleships-in-a-board/419-battleships-in-a-board.py
@@ -1,29 +1,19 @@
 class Solution(object):
     def countBattleships(self, board):
         
-        rows, cols = len(board), len(board[0])
-        
-        num_ships = 0 
-        
         def dfs(x,y):
-            
-            board[x][y] = "."
-            if x+1 < rows and board[x+1][y] == "X":
-                dfs(x+1,y)
-            if x-1 >= 0 and board[x-1][y] == "X":
-                dfs(x-1,y)
-            if y+1 < cols and board[x][y+1] == "X":
-                dfs(x,y+1)
-            if y-1 >= 0 and board[x][y-1] == "X":
-                dfs(x,y-1)
-        
-        
-        
+            board[x][y] = '.'
+            directions = [(1,0),(-1,0),(0,1),(0,-1)]
+            for a,b in directions: 
+                if 0 <= x+a < rows and 0 <= y + b < cols and board[x+a][y+b] == 'X':
+                    dfs(x+a,y+b)
+                    
+    
+        rows, cols = len(board),len(board[0])
+        numShips = 0 
         for x in range(rows):
             for y in range(cols):
-                if board[x][y] == "X":
+                if board[x][y] == 'X':
                     dfs(x,y)
-                    num_ships += 1 
-        
-        return num_ships
-                    
+                    numShips += 1 
+        return numShips 
