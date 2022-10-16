b37514ab7bb90c3035b9c76cf63b704f2fa0abe3
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-10-16 03:06:49 -0400

    Time: 35 ms (60.52%), Space: 13.1 MB (97.83%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,28 +1,8 @@
 class Solution(object):
-    def minimumOperations(self, parcels):
-        # length of parcels
-        n = len(parcels)
-        # count number of days
-        days = 0
-        # to know whether any parcel is left
-        parcelsRemaining = True
-        # loop until parcels to deliver exist
-        while parcelsRemaining:
-            minParcel = float('inf')
-            parcelsRemaining=False
-            # calculate the minimum parcel to be delivered
-            for i in range(n):
-                if parcels[i] == 0:
-                    continue
-                minParcel=min(minParcel,parcels[i])
-                # even if one parcel is non zero then parcels to deliver will be True
-                parcelsRemaining=True
-            # subtract the minimum parcel from all non-zero parcels
-            for i in range(n):
-                if parcels[i]==0:
-                    continue
-                parcels[i]-=minParcel
-            # increment days
-            days+=1
-        # return days(-1 because in last iteration an extra day will be counted)
-        return days-1
+    def minimumOperations(self, nums):
+        res = 0 
+        deliveries = set()
+        for x in nums:
+            if x:
+                deliveries.add(x)
+        return len(deliveries)
