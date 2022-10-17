d822dc3d5bab2621bbc49f3ba9f1e8471886b7f4
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-10-17 05:15:00 -0400

    Time: 1303 ms (48.22%), Space: 13.7 MB (52.60%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def fourSumCount(self, nums1, nums2, nums3, nums4):
+        ans = 0
+        d1 = {}
+        for i in nums1:
+            for j in nums2:
+                if (i + j) in d1:
+                    d1[i + j] += 1
+                else:
+                    d1[i +j] = 1
+        for i in nums3:
+            for j in nums4:
+                target = -(i+j)
+                if target in d1:
+                    ans += d1[target]
+        #print(d1)   
+        return ans
+        
