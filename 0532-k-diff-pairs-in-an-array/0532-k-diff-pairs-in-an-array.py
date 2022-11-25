ade29d7b2355d09722d8fbe19475d08c326bcc29
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-11-25 09:12:24 -0500

    Time: 70 ms (85.17%), Space: 16.9 MB (5.26%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
@@ -13,12 +13,10 @@ class Solution(object):
                 res +=1 
                 pairs.add((x,-1*(k-x)))
                 pairs.add((-1*(k-x),x))
-            
             if (k + x) in differences and (x,k+x) not in pairs:
                 res += 1 
                 pairs.add((x,k+x))
                 pairs.add((k+x,x))
-            
             differences[x] = 1
         return res 
         
