9f8d2b599ca6674f7296a7993adb095df20c80a7
naseemahmed98 <99167518+naseemahmed98@users.noreply.github.com>
2022-11-26 03:26:12 -0500

    Time: 123 ms (69.44%), Space: 15.7 MB (23.59%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def deleteAndEarn(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = Counter(nums)
+        nums = sorted(list(set(nums)))
+        
+        earn1, earn2 = 0,0
+        for i in range(len(nums)):
+            currEarn = nums[i] * count[nums[i]]
+            if i > 0 and nums[i] == nums[i-1] + 1:
+                temp = earn2 
+                earn2 = max(currEarn+earn1, earn2)
+                earn1 = temp
+            
+            else:
+                temp = earn2
+                earn2 = currEarn + earn2
+                earn1 = temp
+            
+           
+        
+        return earn2
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
